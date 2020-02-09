ef1d99d2f52f259997767cd096ff4fc54dd5f26d
Hiroki Kondo <kondo.matr02@gmail.com>
2020-02-09 16:33:51 +0900

    Initial Commit

---
+++ SpeakWriteToDevice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpeakWriteToDevice
+//
+//  Created by 近藤宏輝 on 2020/02/09.
+//  Copyright © 2020 Hiroki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
