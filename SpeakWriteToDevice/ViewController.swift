83576d00f747aab93e7102b52793e6dc770c9bf2
Hiroki Kondo <kondo.matr02@gmail.com>
2020-02-09 16:54:23 +0900

    音声入力許可の機能を実装

--- SpeakWriteToDevice/ViewController.swift
+++ SpeakWriteToDevice/ViewController.swift
@@ -7,20 +7,51 @@
 //
 
 import UIKit
+import Speech
 
 class ViewController: UIViewController {
     
+    private let speechRecoginizer = SFSpeechRecognizer(locale: Locale(identifier: "ja-JP"))!
+    privatevar recognitionTask = SFSpeechRecognitionTask?
     
     @IBOutlet weak var textView: UITextView!
-    
     @IBOutlet weak var recordButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        recordButton.isEnabled = false
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        SFSpeechRecognizer.requestAuthorization { (status) in
+            OperationQueue.main.addOperation {
+                switch status {
+                case .authorized:
+                    //許可
+                    self.recordButton.isEnabled       = true
+                    self.recordButton.backgroundColor = UIColor.systemBlue
+                case .denied:
+                    //拒否
+                    self.recordButton.isEnabled = false
+                    self.recordButton.setTitle("録音許可無し", for: .disabled)
+                case .restricted:
+                    //限定
+                    self.recordButton.isEnabled = false
+                    self.recordButton.setTitle("このデバイスでは無効", for: .disabled)
+                case .notDetermined:
+                    //不明
+                    self.recordButton.isEnabled = false
+                    self.recordButton.setTitle("録音機能が無効", for: .disabled)
+                }
+                
+            }
+        }
+    }
 
+    
     
     @IBAction func recordButtonTapped(_ sender: Any) {
     }
